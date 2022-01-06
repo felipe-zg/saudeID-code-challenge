4710c6f8c6a80af4b124a9c60b31db21fd54264c
felipe-zg <felipe_zeba@outlook.com>
2022-01-06 14:29:10 -0600

    chore: add redux

---
+++ src/store/ducks/todos.duck.ts
@@ -0,0 +1,45 @@
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
+import {RootState} from '..';
+
+type TodosState = {
+	todos: Array<ITodo>;
+	loading: boolean;
+};
+
+export interface ITodo {
+	id: number;
+    user_id: number;
+    title: string;
+    due_date: string;
+}
+
+const INITIAL_STATE: TodosState = {
+    todos: [],
+	loading: false,
+};
+
+const TodosSlice = createSlice({
+	name: 'Todos',
+	initialState: INITIAL_STATE,
+	reducers: {
+		setLoading(state, {payload}: PayloadAction<boolean>) {
+			return {
+				...state,
+				loading: payload,
+			};
+		},
+		setTodos(state, {payload}: PayloadAction<ITodo[]>) {
+			return {
+				...state,
+				todos: payload,
+			};
+		},
+	},
+});
+
+
+export const {setLoading, setTodos} = TodosSlice.actions;
+
+export default TodosSlice.reducer;
+
+export const selectTodosState = (state: RootState) => state.todos;
