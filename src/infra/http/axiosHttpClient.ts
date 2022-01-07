8056ec8ada59073b8c6acbfe0f8ae236603b4b86
felipe-zg <felipe_zeba@outlook.com>
2022-01-07 17:27:19 -0600

    refactor: transform HttpClient into a singgleton

--- src/infra/http/axiosHttpClient.ts
+++ src/infra/http/axiosHttpClient.ts
@@ -1,13 +1,28 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosInstance, AxiosResponse } from 'axios';
 import { HttpGetClient, HttpGetClientParams } from '../../data/protocols/http/httpGetClient';
 import { HttpPostClientParams } from '../../data/protocols/http/httpPostClient';
 import { HttpResponse } from '../../data/protocols/http/httpResponse';
 
 export class AxiosHttpClient<R = unknown> implements HttpGetClient<R> {
+    private static instance: AxiosHttpClient;
+
+    static getInstance() {
+        if(!this.instance) {
+            this.instance = new AxiosHttpClient();
+        }
+        return this.instance;
+    }
+
+    private httpClient: AxiosInstance = axios.create({
+        baseURL: process.env.REACT_APP_API_GATEWAY_URL,
+        timeout: 30000,
+        headers: { 'Access-Control-Allow-Origin': '*' },
+    });
+
     async get(params: HttpGetClientParams): Promise<HttpResponse<R>> {
         let axiosResponse: AxiosResponse<R>;
         try {
-            axiosResponse = await axios.get(params.url);
+            axiosResponse = await this.httpClient.get(params.url);
         } catch (error) {
             axiosResponse = error.response;
         }
@@ -17,7 +32,7 @@ export class AxiosHttpClient<R = unknown> implements HttpGetClient<R> {
     async post(params: HttpPostClientParams): Promise<HttpResponse<any>> {
         let axiosResponse: AxiosResponse<any>;
         try {
-            axiosResponse = await axios.post(params.url, params.body);
+            axiosResponse = await  this.httpClient.post(params.url, params.body);
         } catch (error) {
             axiosResponse = error.response;
         }
@@ -27,7 +42,7 @@ export class AxiosHttpClient<R = unknown> implements HttpGetClient<R> {
     async delete(params: HttpPostClientParams): Promise<HttpResponse<any>> {
         let axiosResponse: AxiosResponse<any>;
         try {
-            axiosResponse = await axios.delete(params.url, params.body);
+            axiosResponse = await  this.httpClient.delete(params.url, params.body);
         } catch (error) {
             axiosResponse = error.response;
         }
